fb08871d5f67c352624978b95d5237f63cf8f175
Martin Karcher <mkarcher@Martins-MacBook-Pro.local>
2022-10-13 21:35:41 +0200

    Added my first programs in C and C++

---
+++ myFirstCProgam.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello world\n");
+    double number1 = 10;
+    double number2 = 20;
+    double result = number1 + number2;
+
+    printf("The result is: %f", result);
+
+    return 0;
+}
---
+++ myFirstCppProgram.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <cstring>
+#include <cstdlib>
+
+using namespace std ;
+int main()
+{
+    cout << "Hello world!" << endl ;
+
+}
